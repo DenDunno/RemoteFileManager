52c51232eb35aa0ae1bccf0d4ddbfe3b4691da98
Denis Bondar <60221683+Dunno-official@users.noreply.github.com>
2021-03-09 19:17:07 +0200

    view

--- RemoteFileManager/MainWindow.xaml.cs
+++ RemoteFileManager/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,47 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RemoteFileManager
+
+namespace Server
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string _savePath = "C:";
+
         public MainWindow()
         {
             InitializeComponent();
+            ShowFiles("C:\\Program Files\\Windows Media Player");
         }
+
+
+        private void ShowFiles(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            List<DirectoryInfo> allDirectories = directory.GetDirectories().ToList();
+            List<FileInfo> allFileInfoes = directory.GetFiles().ToList();
+
+            allDirectories.ForEach(dir => _listBox.Items.Add(dir));
+            allFileInfoes.ForEach(fileInfo => _listBox.Items.Add(fileInfo));
+        }
+
+
+        private void DownloadFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+
+
+        private void SavePath_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Your save path: " + "\"" + _savePath + "\"");
+        }
+
+
+        private void DirectoryWasChosen(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(sender.ToString());
+        }
+        
     }
 }
